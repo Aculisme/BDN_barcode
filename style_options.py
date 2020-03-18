b475e30c756dd037c2b40437cb02e02aafc7de50
Luca <luca.mehl@gmail.com>
2020-03-18 17:11:40 +0100

    added requirements, removed MIT liscence, added barcode style system

---
+++ style_options.py
@@ -0,0 +1,64 @@
+barcode_style_options = {
+    "crimson_bg": {
+        "background":'crimson',#"rgba(0,0,0,0)", | darkorange | peru | lightpink | mediumvioletred | lightsalmon | lightcoral
+        "foreground":"black",
+        "quiet_zone":1,
+        "text_distance":1,
+        "module_height":8 # default is 15mm
+        },
+    "white_bg": {
+        "background":'white',#"rgba(0,0,0,0)", | darkorange | peru | lightpink | mediumvioletred | lightsalmon | lightcoral
+        "foreground":"black",
+        "quiet_zone":1,
+        "text_distance":1,
+        "module_height":8 # default is 15mm
+    },
+    "rgb_test": {
+        "background":"rgb(100,100,200)",#"rgba(0,0,0,0)", | darkorange | peru | lightpink | mediumvioletred | lightsalmon | lightcoral
+        "foreground":"black",
+        "quiet_zone":1,
+        "text_distance":1,
+        "module_height":8 # default is 15mm
+    },
+    "peach": {
+        "background":"rgb(249,181,100)",#"rgba(0,0,0,0)", | darkorange | peru | lightpink | mediumvioletred | lightsalmon | lightcoral
+        "foreground":"black",
+        "quiet_zone":1,
+        "text_distance":1,
+        "module_height":8 # default is 15mm
+    },
+    "golden_yellow": {
+        "background":"rgb(254,142,18)",#"rgba(0,0,0,0)", | darkorange | peru | lightpink | mediumvioletred | lightsalmon | lightcoral
+        "foreground":"black",
+        "quiet_zone":1,
+        "text_distance":1,
+        "module_height":8 # default is 15mm
+    },
+    "red": {
+        "background":"rgb(237,6,7)",#"rgba(0,0,0,0)", | darkorange | peru | lightpink | mediumvioletred | lightsalmon | lightcoral
+        "foreground":"black",
+        "quiet_zone":1,
+        "text_distance":1,
+        "module_height":8 # default is 15mm
+    },
+    "pink": {
+        "background":"rgb(228,57,128)",#"rgba(0,0,0,0)", | darkorange | peru | lightpink | mediumvioletred | lightsalmon | lightcoral
+        "foreground":"black",
+        "quiet_zone":1,
+        "text_distance":1,
+        "module_height":8 # default is 15mm
+    },
+    "cotton_candy": {
+        "background":"rgb(235,163,178)",#"rgba(0,0,0,0)", | darkorange | peru | lightpink | mediumvioletred | lightsalmon | lightcoral
+        "foreground":"black",
+        "quiet_zone":1,
+        "text_distance":1,
+        "module_height":8 # default is 15mm
+    }
+}
+
+ticket_style_options = {
+    "barcode_only": {
+        "left_offset":-100
+    }
+}
--- test_barcode.py
+++ test_barcode.py
@@ -3,20 +3,17 @@ from pprint import pprint as pp
 import barcode
 from barcode.writer import ImageWriter
 
+# Style imports
+from style_options import barcode_style_options
 
-def generate_barcode(ticket_id):
+
+def generate_barcode(ticket_id,**barcode_style):
     barcode_class = barcode.get_barcode_class("code39")
     generated_barcode = barcode_class(ticket_id,add_checksum=False, writer=ImageWriter())
     pp(generated_barcode.get_fullcode())
     barcode_filename = generated_barcode.save(
-        "barcodes/{}".format(ticket_id), 
-        options={
-            "background":'white',#"rgba(0,0,0,0)", | darkorange | peru | lightpink | mediumvioletred | lightsalmon | lightcoral
-            "foreground":"black",
-            "quiet_zone":1,
-            "text_distance":1,
-            "module_height":8 # default is 15mm
-            }
+        filename="barcodes/{}".format(ticket_id), 
+        options = barcode_style
         )
     pp(barcode_filename) ###
     return barcode_filename
@@ -32,7 +29,7 @@ def coordinates_centered(fg_barcode,bg_template):
     return coodinates_centered
 
 
-def merge_barcode(ticket_id, barcode_filename, template_filename):
+def merge_barcode(ticket_id, barcode_filename, template_filename,w_offset_mult=0):
     ticket_filename = "tickets/{}_ticket.png".format(ticket_id)
     fg_barcode = Image.open(barcode_filename).convert("RGBA")
     bg_template = Image.open(template_filename)
@@ -45,7 +42,8 @@ def merge_barcode(ticket_id, barcode_filename, template_filename):
         )
     )
     h, w = coordinates_centered(fg_barcode,bg_template)
-    bg_template.paste(fg_barcode, (w-50,h), fg_barcode)
+    w_offset = int(w*w_offset_mult)
+    bg_template.paste(fg_barcode, (w-w_offset,h), fg_barcode)
     bg_template.save(ticket_filename,format="png")
     pp(ticket_filename)
     return ticket_filename
@@ -55,12 +53,16 @@ def merge_barcode(ticket_id, barcode_filename, template_filename):
 if __name__=="__main__":
     start_number = 1800
     number_to_print = 1
-    template_filename = "templates/blank_bdn_background.png"
+    # template_filename = "templates/blank_bdn_background.png"
+    template_filename = "templates/flipped_blank_bdn_background.png"
 
+    barcode_style = barcode_style_options['cotton_candy']
+    pp(barcode_style)
     for ticket_id in range(start_number,start_number+number_to_print):
         ticket_id = str(ticket_id).zfill(6)
 
-        barcode_filename = generate_barcode(ticket_id)
-        merge_barcode(ticket_id, barcode_filename, template_filename)
+        barcode_filename = generate_barcode(ticket_id,**barcode_style)
+        
+        merge_barcode(ticket_id, barcode_filename, template_filename,w_offset_mult=0.0)
 
 
